40e6375a47f40f1a4fc5a831a4db49a81bbc9149
Saad Hamid <saad@robots.ox.ac.uk>
2019-03-05 15:53:04 +0000

    Got RRT sort of working

---
+++ route_planner/BoundPoles.m
@@ -0,0 +1,16 @@
+function boxes = BoundPoles(pole_positions)
+% Returns a matrix where each row is a box around the poles.
+% boxes is [x_bottom_left, y_bottom_left, width, height]
+
+width = 200;
+height = 200;
+
+sz = size(pole_positions);
+num_poles = sz(2)/2;
+boxes = zeros(num_poles, 4);
+for i = 1:1:num_poles
+    boxes(i,:) = [pole_positions(i)-width/2, pole_positions(i+1)-height/2,...
+                  width, height];
+end
+
+end
