0b634ff8af19dca929aa7fc8acb78453ad5da479
Kaur Aare Saar <kauraare@gmail.com>
2019-03-05 11:57:30 +0000

    convert between reference frames

---
+++ wheel_controller/Local2Global.m
@@ -0,0 +1,7 @@
+function target_global = Local2Global(robot_global, target_local)
+    angle = target_local(3) + robot_global(3);
+    x = -robot_global(3);
+    R = [cos(x), -sin(x); sin(x) cos(x)];
+    displacement_global = R * target_local(1:2);
+    target_global = [displacement_global + robot_global(1:2) ; angle]; 
+end
