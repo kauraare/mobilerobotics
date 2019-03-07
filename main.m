c332e9cd9848c841575b25c39b04b53d5fd2ecbf
Kaur Aare Saar <kauraare@gmail.com>
2019-03-07 14:15:59 +0000

    exponential moving average and check if we've reached target

--- main.m
+++ main.m
@@ -17,14 +17,16 @@ pause(4.0); % give mexmoos a chance to connect (important!)
 
 % First tell it not to move at all
 SendSpeedCommand(0, 0, config.control_channel)
-found_target_flag = 0;
 counter = 1;
 state_vector = [1; 4; 0];
 state_cov = ones(3,3);
 accum_time = 1e5;
 req_new_carrot = 1;
 plan_flag = 1;
-target_location = Local2Global([0;0;0],[3;0;0]);  
+decay = 5; % decay for exponential moving average for target
+target_distance_threshold = 0.3; % distance to target when to stop
+reached_target = false;
+target_location_array = Local2Global([0;0;0],[3;0;0]);
 while true
     % Fetch latest messages from mex-moos
     pause(0.01)
@@ -32,33 +34,45 @@ while true
     scan = GetLaserScans(mailbox, config.laser_channel, true);
     stereo_images = GetStereoImages(mailbox, config.stereo_channel, true);
     wheel_odometry_all = GetWheelOdometry(mailbox, ...
-                                          config.wheel_odometry_channel, ...
-                                          false);
+        config.wheel_odometry_channel, ...
+        false);
     wheel_odometry = ComposeWheelOdom(wheel_odometry_all);
-
-%   TARGET DETECTION
-	found_target = TargetDetector(config, stereo_images);
-%     if we have found a target update the target location!
-    if ~(isnan(found_target) == 0)
-%         Do we need tl_o convert target location into global frame?
-        disp("~ FOUND THE TARGET AT ~")
-        found_target 
-        local_target(1) = found_target(1) * cos(found_target(2));
-        local_target(2) = found_target(1) * sin(found_target(2));
-        local_target(3) = found_target(2);
-        target_location = Local2Global(state_vector(1:3)',local_target');
-    end
-   
     
-%   POLE DETECTION
-	[ranges, bearings] = DetectPoles(scan);
-
-%   SLAM
+    %   TARGET DETECTION
+    found_target = TargetDetector(config, stereo_images);
+    disp("~ FOUND THE TARGET AT ~")
+    disp(found_target)
+    
+    % Convert to global
+    local_target(1) = found_target(1) * cos(found_target(2));
+    local_target(2) = found_target(1) * sin(found_target(2));
+    local_target(3) = found_target(2);
+    
+    % Append array
+    target_location_array(:,counter+1) = Local2Global(state_vector(1:3)',local_target');
+    
+    % Find moving average
+    weights = exp(linspace(0, -counter/decay, counter+1));
+    weights(any(isnan(local_target),1)) = 0; % if localtarget is nan set weight to 0
+    weights(1)=1e-100; % to ensure at least one is positive
+    weights=weights/sum(weights);
+    target_location = nansum(target_location_array.*weights,2);
+    
+    % Check if we have reached target
+    
+    if any(isnan(local_target(:,end))) & norm(target_location(1:2)-state_vector(1:2)) < target_distance_threshold
+        reached_target=true;
+    end
+    
+    %   POLE DETECTION
+    [ranges, bearings] = DetectPoles(scan);
+    
+    %   SLAM
     [state_vector, state_cov] = SLAMUpdate(wheel_odometry, ...
-                                           [ranges, bearings], ...
-                                           state_vector, state_cov);
-        
-%   ROUTE PLANNING
+        [ranges, bearings], ...
+        state_vector, state_cov);
+    
+    %   ROUTE PLANNING
     if mod(counter, 100)
         plan_flag = 1;
     end
@@ -72,8 +86,8 @@ while true
         carrot = carrots(end-carrot_num,:);
         carrot_num = carrot_num + 1;
     end
-
-%   MOVE
+    
+    %   MOVE
     req_new_carrot = TurnDriveTurn(config, state_vector, carrot);
     
     counter = counter + 1;
