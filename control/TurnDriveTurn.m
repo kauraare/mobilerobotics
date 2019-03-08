5da84ec6e9a35dc5a09b4aa376da99aa1be02acd
Kaur Aare Saar <kauraare@gmail.com>
2019-03-08 10:51:55 +0000

    fix and clean turn drive turn controller

--- control/TurnDriveTurn.m
+++ control/TurnDriveTurn.m
@@ -1,59 +1,50 @@
 function request_new_carrot = TurnDriveTurn(husky_config, state_vector, carrot)
 % Make the robot move to the carrot.
+
+% Set parameters
 angle_threshold = pi/12;
 range_threshold = 0.25;
-
-carrot(3) = atan(carrot(2) / carrot(1));
-local_carrot = Global2Local(state_vector(1:3), carrot');
-local_carrot(3) = carrot(3) - state_vector(3);
-displacement = carrot(1:2) - state_vector(1:2)';
-range = sqrt(sum(displacement.^2));
-local_carrot(1) = range*cos(local_carrot(3));
-local_carrot(2) = range*sin(local_carrot(3));
-
 angular_vel = pi/6; % rad/sec
 vel = 0.1; % m/s
+request_new_carrot = 0;
 
-% delta_angle = local_carrot(3);
-% delta_x = sqrt(local_carrot(1)^2 + local_carrot(2)^2);
-%
+% Calculate carrot in local polar coordinates
+carrot(3) = atan2(carrot(2)-state_vector(2), carrot(1)-state_vector(1)); % align rotation at carrot with displacement
+local_carrot = Global2Local(state_vector(1:3), carrot');
+delta_angle = local_carrot(3);
+delta_range = norm(local_carrot(1:2));
+
+
 % time_turn = angular_vel / delta_angle;
 % time_drive = delta_x / vel;
 
 % GetWheelOdometry(mailbox, config.wheel_odometry_channel, true);
 % last_source_timestamp = wheel_odometry.source_timestamp;
 
+% wheel_odometry = GetWheelOdometry(mailbox, ...
+%     config.wheel_odometry_channel, ...
+%     true);
+% if wheel_odometry.source_timestamp ~= last_source_timestamp
+%     tbc = [wheel_odometry.x; wheel_odometry.y; wheel_odometry.yaw];
+%     tab = tcomp(tab,tbc);
+%     delta_angle = local_carrot(3) - tab(3);
+%     delta_range = sqrt(local_carrot(1)^2 + local_carrot(2)^2) ...
+%         - sqrt(tab(1)^2 + tab(2)^2);
+% end
 
-delta_angle = local_carrot(3);
-delta_range = sqrt(local_carrot(1)^2 + local_carrot(2)^2);
-
-request_new_carrot = 0;
-
-%     wheel_odometry = GetWheelOdometry(mailbox, ...
-%                                       config.wheel_odometry_channel, ...
-%                                       true);
-%     if wheel_odometry.source_timestamp ~= last_source_timestamp
-%         tbc = [wheel_odometry.x; wheel_odometry.y; wheel_odometry.yaw];
-%         tab = tcomp(tab,tbc);
-%         delta_angle = local_carrot(3) - tab(3);
-%         delta_range = sqrt(local_carrot(1)^2 + local_carrot(2)^2) ...
-%                       - sqrt(tab(1)^2 + tab(2)^2);
-%     end
-
-if delta_angle > angle_threshold
+if abs(delta_angle) > angle_threshold
     'Turning'
     SendSpeedCommand(0, angular_vel, husky_config.control_channel);
     pause(0.01);
 elseif delta_range > range_threshold
-    'Driving'
+    'Driving forward'
     SendSpeedCommand(vel, 0, husky_config.control_channel);
     pause(0.01);
 else
     request_new_carrot = 1;
     SendSpeedCommand(0, 0, husky_config.control_channel);
     pause(0.01);
-    return
-    
+    return 
 end
 %     last_source_timestamp = wheel_odometry.source_timestamp;
 
